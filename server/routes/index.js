e155129ace3a783bb6bc16b11a4c40fe45357fce
Diana V Rodríguez L <dvrodriguez.22@est.ucab.edu.ve>
2024-12-03 16:13:08 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const { controller } = require("../controllers/controller");
+
+//Se crea clase Controller y se llaman a los métodos de esa clase
+var rep = new SimulacionReportesCont();
+
+module.exports = router;
